7517249e8f9d62b8d2739c93f81132a7d915d136
rakibul263 <rakibulhasanshuvo206@gmail.com>
2026-05-23 00:33:30 +0600

    DevPuls config and database init added

---
+++ src/database/db.ts
@@ -0,0 +1,37 @@
+import { Pool } from "pg";
+import config from "../config";
+
+const pool = new Pool({ connectionString: config.database_url });
+
+export const initDatabase = async () => {
+  try {
+    const createUsersTable = `
+            CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(255) NOT NULL,
+            email VARCHAR(255) UNIQUE NOT NULL,
+            password VARCHAR(255) NOT NULL,
+            role VARCHAR(50) DEFAULT 'contributor' CHECK (role IN ('contributor', 'maintainer')),
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );
+  `;
+    const createIssuesTable = `
+            CREATE TABLE IF NOT EXISTS issues (
+            id SERIAL PRIMARY KEY,
+            title VARCHAR(150) NOT NULL,
+            description TEXT NOT NULL,
+            type VARCHAR(50) NOT NULL CHECK (type IN ('bug', 'feature_request')),
+            status VARCHAR(50) DEFAULT 'open' CHECK (status IN ('open', 'in_progress', 'resolved')),
+            reporter_id INT NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );
+  `;
+    console.log("Database tables create successfully.");
+  } catch (error: any) {
+    console.error("Error initializing database tables:", error);
+  }
+};
+
+export default pool;
