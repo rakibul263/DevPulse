73ccc3141c22e1af70bd7a90b0c8739838117f39
rakibul263 <rakibulhasanshuvo206@gmail.com>
2026-05-23 14:23:08 +0600

    app router connection fixed

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import config from "./config";
+import issueRouter from "./modules/issues/issues.routes";
+import authRouter from "./modules/auth/auth.router";
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/api/auth");
-app.use("api/issues");
+app.use("/api/auth", authRouter);
+app.use("api/issues", issueRouter);
 
 app.listen(config.port, () => {
   console.log(`server is running at port ${config.port}`);
